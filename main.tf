f434faae7aeb56657fda1c1e6dc9a0f7294d5a82
svanu07 <49926814+svanu07@users.noreply.github.com>
2019-05-16 19:20:46 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,30 +23,3 @@ resource "scaleway_volume" "wordpress-iot" {
   size_in_gb = 50
   type       = "l_ssd"
 }
-
-resource "scaleway_security_group" "http" {
-  name        = "http"
-  description = "allow HTTP and HTTPS traffic"
-}
-
-resource "scaleway_security_group_rule" "http_accept" {
-  security_group = "${scaleway_security_group.http.id}"
-
-  action    = "accept"
-  direction = "inbound"
-  ip_range  = "0.0.0.0/0"
-  protocol  = "TCP"
-  port      = 80
-}
-
-resource "scaleway_security_group_rule" "https_accept" {
-  security_group = "${scaleway_security_group.http.id}"
-
-  action    = "accept"
-  direction = "inbound"
-  ip_range  = "0.0.0.0/0"
-  protocol  = "TCP"
-  port      = 443
-}
-
-
